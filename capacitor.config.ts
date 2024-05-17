36ac6285896fd8561c440b3eb4e3cd829e74b7cc
risthian-P <cristhian.panora@gmail.com>
2024-05-16 23:35:01 -0500

    first commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'photo-gallery',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000, // Duración en milisegundos
+      backgroundColor: '#de0f17', // Color de fondo (opcional)
+      showSpinner: true, // Mostrar un spinner (opcional)
+      androidSpinnerStyle: 'small'
+    }
+  }
 };
 
 export default config;
